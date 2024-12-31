468ed9b7dbd48513dbd423c27a604c0270fd026c
Jonathan <jonathan66652@gmail.com>
2024-12-31 17:11:58 +0800

    更新1-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -35,6 +35,7 @@
     WHERE email = 'opacity0@hexschooltest.io';
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
+    select COUNT(*) from "USER";
 
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
 
